18fcf4a947d59d1d800ceebaa96b15fc0325b32c
Joanna Stiller <joannastiller@yahoo.com>
2019-01-18 16:33:10 +0100

    Updating scrolling function

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const burger = document.querySelector("i.fa-bars");
 const times = document.querySelector("i.fa-times");
-const nav = document.querySelector('nav');
+const nav = document.querySelector('section.top nav');
 
 burger.addEventListener('click', function () {
-    burger.classList.toggle('active');
-    times.classList.toggle('active');
-    nav.classList.toggle('active');
+    burger.classList.remove('active');
+    times.classList.add('active');
+    nav.classList.add('active');
+})
+
+times.addEventListener('click', function () {
+    times.classList.remove('active');
+    burger.classList.add('active');
+    nav.classList.remove('active');
 })
